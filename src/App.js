12e9f26c10f9f4eaa99125824f650af3303925a0
Gabriele Luz <gabrieleluz@infojr.com.br>
2019-12-16 19:51:19 -0300

    updates default style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+
 import './App.css';
+
+import Layout from './containers/Layout';
 
 class App extends Component {
 
   render() {
     return(
-      <div></div>
+      <Layout />
     );
   }
 }
