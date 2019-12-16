ac94e506866cbf59d9b73bb94ddf28cad9a22b4a
Gabriele Luz <gabrieleluz@infojr.com.br>
2019-12-16 19:54:22 -0300

    adds layout component

---
+++ src/containers/Layout/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import styles from './styles.module.css';
+
+class Layout extends Component {
+
+  render() {
+    return(
+      <div className={styles.Layout}>
+      </div>
+    );
+  }
+}
+
+export default Layout;
