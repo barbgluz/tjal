111972770548bf61f41ff78f289f71f525f0192d
Gabriele Luz <gabrieleluz@infojr.com.br>
2019-12-17 00:42:58 -0300

    dynamically displays motions @ process component

--- src/containers/Process/index.js
+++ src/containers/Process/index.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
 
+import motions from '../../data/motions';
+
 import styles from './styles.module.css';
+
+import Motion from '../../components/Motion';
 
 class Process extends Component {
 
   render() {
+
+    let movimentacoes = <p>Não há movimentações.</p>
+    movimentacoes = motions.map(motion => {
+      return(
+        <Motion title={motion.title} date={motion.date} content={motion.content} />
+      )
+    })
+
     return(
       <div className={styles.Process}>
         <div className={styles.Main}>
-          <h1>Processo n. </h1>
+          <h1>Processo n. 0067154-55.2010.8.02.0001 </h1>
 
           <div className={styles.Movimentacoes}>
-            <p>movimentacao</p>
+            { movimentacoes }
           </div>
         </div>
 
