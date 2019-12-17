36cbd3f015ee9b7fdaaf4587449087cb129dae4d
Gabriele Luz <gabrieleluz@infojr.com.br>
2019-12-17 12:04:33 -0300

    adds parts component

---
+++ src/data/parts.js
@@ -0,0 +1,36 @@
+var data = [
+	{
+		nome: "Manuella de Melo Morais Borges",
+		tipo: "Autora",
+		representantes: [
+			{
+				nome: "Tiago Pereira Barros",
+				tipo: "Advogado"
+			},
+			{
+				nome: "José Areias Bulhões",
+				tipo: "Advogado"
+			},
+			{
+				nome: "Thaís Malta Bulhões",
+				tipo: "Advogado"
+			},
+		]
+	},
+	{
+	nome: "Unimed Maceió",
+	tipo: "Réu",
+	representantes: [
+		{
+			nome: "Caio Cesar de Oliveira Amorim Candido",
+			tipo: "Advogado"
+		},
+		{
+			nome: "Gustavo Uchôa Castro",
+			tipo: "Advogado"
+		},
+	]
+},
+]
+
+export default data;
