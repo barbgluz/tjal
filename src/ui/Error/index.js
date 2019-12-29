5d7a89481a26bedfd19a815e7f424d087106d701
Gabriele Luz <gabrieleluz@infojr.com.br>
2019-12-29 17:26:58 -0300

    minor styles/responsivity adjustments

--- src/ui/Error/index.js
+++ src/ui/Error/index.js
@@ -6,7 +6,7 @@ import image from '../../containers/Search/background.jpg'
 const error = () => {
   return(
     <div className={styles.Error}>
-      <img src={image} className={styles.ErrorImg}/>
+      <img src={image} alt="#" className={styles.ErrorImg}/>
       <div>
         <h1>ERRO</h1>
         <p>Não encontramos seu processo. <br />Verifique o numero de processo e tente novamente.</p>
